5c1a3b34d1056ba16b0ed8cc9678e420036b513a
Kaisa Lumiste <lumistekaisa@gmail.com>
2024-09-13 20:59:27 +0300

    week 2

---
+++ Week2IFStatements/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("Enter your last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Enter your gender (f/m/n):");
+char userGender = Char.Parse(Console.ReadLine());
+
+string greeting;
+
+if(userGender == 'f')
+{
+    greeting = "Ms.";
+}
+else if (userGender == 'm') 
+{
+    greeting = "Mr.";
+}
+else 
+{
+    greeting = "";
+}
+
+
+Console.WriteLine($"Hello, {greeting} {lastName}!");
+
+Console.WriteLine("Enter your year of birth:");
+
+int usersYoB = Int32.Parse(Console.ReadLine());
+
+int currentYear = DateTime.Now.Year;
+
+int userAge = currentYear - usersYoB;
+
+//Console.WriteLine($"Current year: {currentYear}");
+
+//int userAge = Int32.Parse(Console.ReadLine());
+
+if (userAge >= 13)
+{
+    Console.WriteLine($"You are {userAge} - old enough to have an account");
+}
+else
+{
+    Console.WriteLine($"You are {userAge} - too young to have an account");
+}
